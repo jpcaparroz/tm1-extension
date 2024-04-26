ab680c0730b969c9eea6f7b66fc600bafd19d86d
Filipe Carvalho <filiperoc@hotmail.com>
2024-04-26 20:38:39 -0300

    feat: adding tree-provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,31 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
-import * as tm1js from 'tm1js';
 import * as credentials from './assets/examples/applications.json';
-import { connectToTM1 } from './services/authenticator';
+import { authenticate } from './services/authenticator';
+import { TM1Service } from 'tm1js';
+import { TreeProvider } from './services/tree-provider';
 
-// async function createTreeViewNodes(jsonContent: any): Promise<vscode.TreeItem[]> {
+export async function activate(context: vscode.ExtensionContext) {
+    // let disposable = vscode.commands.registerCommand('tm1-extension.tm1Test', async () => {
+    //     vscode.window.showInformationMessage('Hello, World!');
+    //     const tm1Service: TM1Service = await authenticate(credentials.GO_New_Stores);
+        
+    //     // Lista de tipos de objetos e os respectivos métodos para obter os nomes
+    //     const typesOfObjects = [
+    //         { name: 'Cubes', method: 'getAllModelNames' },
+    //         { name: 'Cubes', method: 'getAllControlNames' },
+    //         { name: 'Dimensions', method: 'getAllNames' },
+    //         { name: 'Processes', method: 'getAllNames' },
+    //         { name: 'Chores', method: 'getAllNames' }
+    //     ];
 
-// 	// tm1js.TM1Service.connect(credentials.)
-// 	const names = jsonContent.value.map((item: any) => item.Name);
-
-// 	return names.map((name: string) => {
-// 		return new vscode.TreeItem(name, vscode.TreeItemCollapsibleState.Collapsed);
-// 	});
-// }
-
-
-// class TreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
-// 	constructor(private readonly extensionPath: string, private readonly fileName: string) { }
-
-// 	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
-// 		return element;
-// 	} 
-
-// 	getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
-// 		if (!element) {
-// 			// const filePath = path.join(this.extensionPath, 'src', 'assets', 'examples', this.fileName);
-// 			const jsonContent = credentials;
-
-// 			const treeNodes = createTreeViewNodes(jsonContent);
-
-// 			return Promise.resolve(treeNodes);
-// 		}
-// 		return Promise.resolve([]);
-// 	}
-// }
-
-// export function activate(context: vscode.ExtensionContext) {
-// 	// const processesDataProvider = new TreeDataProvider(context.extensionPath, 'processes.json');
-// 	// vscode.window.createTreeView('package-processes', { treeDataProvider: processesDataProvider });
-
-// 	const cubesDataProvider = new TreeDataProvider(context.extensionPath, 'cubes.json');
-// 	vscode.window.createTreeView('package-cubes', { treeDataProvider: cubesDataProvider });
-// }
-
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('tm1-extension.tm1Test', () => {
-        vscode.window.showInformationMessage('Hello, World!');
-
-    });
-    context.subscriptions.push(disposable);
-    connectToTM1(credentials.GO_New_Stores)
+    //     // Criar provedores de árvore para cada tipo de objeto
+    //     typesOfObjects.forEach(async ({ name, method }) => {
+    //         const items = await tm1Service[name.toLowerCase()][method]();
+    //         const treeProvider = new TreeProvider(items);
+    //         vscode.window.createTreeView(`package-${name.toLowerCase()}`, { treeDataProvider: treeProvider });
+    //     });
+    // });
+    // context.subscriptions.push(disposable);
 }
 
 export function deactivate() { }
