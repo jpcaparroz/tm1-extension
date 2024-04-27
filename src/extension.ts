3b8821946ac3aef572eaefe8d424fa1a3d211c8b
Filipe Carvalho <filiperoc@hotmail.com>
2024-04-27 16:17:59 -0300

    feat: tree item clickable

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,44 @@
 import * as vscode from 'vscode';
 import * as credentials from './assets/examples/applications.json';
 import { authenticate } from './services/authenticator';
-import { TM1Service } from 'tm1js';
-import { TreeProvider } from './services/tree-provider';
+import { CubeService, TM1Service } from 'tm1js';
+import { TreeItem, TreeProvider } from './services/tree-provider';
+import { typesOfObjects } from './services/object-types-call';
+import * as path from 'path';
+import { createAndOpenFile } from './services/create-file';
 
 export async function activate(context: vscode.ExtensionContext) {
-    // let disposable = vscode.commands.registerCommand('tm1-extension.tm1Test', async () => {
-    //     vscode.window.showInformationMessage('Hello, World!');
-    //     const tm1Service: TM1Service = await authenticate(credentials.GO_New_Stores);
-        
-    //     // Lista de tipos de objetos e os respectivos métodos para obter os nomes
-    //     const typesOfObjects = [
-    //         { name: 'Cubes', method: 'getAllModelNames' },
-    //         { name: 'Cubes', method: 'getAllControlNames' },
-    //         { name: 'Dimensions', method: 'getAllNames' },
-    //         { name: 'Processes', method: 'getAllNames' },
-    //         { name: 'Chores', method: 'getAllNames' }
-    //     ];
+    let loadTree = vscode.commands.registerCommand('tm1-extension.loadTree', async () => {
+        const tm1Service: TM1Service = await authenticate(credentials.GO_New_Stores);
+        typesOfObjects.forEach(async ({ name, method, control }) => {
+            const items = await tm1Service[name][method]();
+            const treeProvider = new TreeProvider(items);
+            const viewId = control ? `package-control-${name}` : `package-${name}`;
+            vscode.window.createTreeView(viewId, { treeDataProvider: treeProvider });
+        });
+    });
 
-    //     // Criar provedores de árvore para cada tipo de objeto
-    //     typesOfObjects.forEach(async ({ name, method }) => {
-    //         const items = await tm1Service[name.toLowerCase()][method]();
-    //         const treeProvider = new TreeProvider(items);
-    //         vscode.window.createTreeView(`package-${name.toLowerCase()}`, { treeDataProvider: treeProvider });
-    //     });
-    // });
-    // context.subscriptions.push(disposable);
+    let treeItemClick = vscode.commands.registerCommand('tm1-extension.treeItemClick', async (treeItem: TreeItem) => {
+        const tm1Service: TM1Service = await authenticate(credentials.GO_New_Stores);
+        console.log(treeItem.label);
+        
+        const process = await tm1Service.processes.get(treeItem)
+        const content = `#Section Prolog\n${process.prologProcedure}\n
+            #Section Metadata\n${process.metadataProcedure}\n
+            #Section Data\n${process.dataProcedure}\n
+            #Section Epilog\n${process.epilogProcedure}
+        `;
+        const fileName = `${treeItem}.pro`;
+        const folderPath = path.join(context.extensionPath, 'src', 'temp');
+        const filePath = folderPath ? path.join(folderPath, fileName) : '';
+
+        if (filePath) {
+            await createAndOpenFile(filePath, content);
+        } else {
+            vscode.window.showErrorMessage('Unable to create file. No workspace folder found.');
+        }
+    });
+    context.subscriptions.push(treeItemClick);
 }
 
 export function deactivate() { }
