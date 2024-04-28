55fbc0a16d72f24ea0282ac6e34afdb7292070e7
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2024-04-28 12:38:05 -0300

    OOP on tree-service

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,16 @@
-import * as vscode from 'vscode';
-import * as credentials from './assets/examples/applications.json';
-
-import { authenticate } from './services/authenticator';
-
+import { ExtensionContext, commands } from 'vscode';
 import { TM1Service } from 'tm1js';
-
-import { CubeItem, CubeItemProvider } from './objects/cube-object';
-import { ProcessItem, ProcessItemProvider } from './objects/process-object';
-import { DimensionItem, DimensionItemProvider } from './objects/dimension-object';
-import { ChoreItem, ChoreItemProvider } from './objects/chore-object';
+import * as credentials from './assets/examples/applications.json';
+import { authenticate } from './services/authenticator';
+import { TreeService, ItemProviderType } from './services/tree-service';
 
 const connection = credentials.planner
 
-export async function activate(context: vscode.ExtensionContext) {
-    let loadAllTree = vscode.commands.registerCommand('tm1-extension.loadTree', async () => {
-        const tm1Service: TM1Service = await authenticate(connection);
+export async function activate(context: ExtensionContext) {
+    const tm1Service: TM1Service = await authenticate(connection);
+    const treeSerivce = new TreeService(tm1Service);
 
-        const cubeItemProvider = new CubeItemProvider(tm1Service);
-        const dimensionItemProvider = new DimensionItemProvider(tm1Service);
-        const processeItemProvider = new ProcessItemProvider(tm1Service);
-        const choreItemProvider = new ChoreItemProvider(tm1Service);
-
-        vscode.window.createTreeView<CubeItem>('package-cubes', { treeDataProvider: cubeItemProvider });
-        vscode.window.createTreeView<DimensionItem>('package-dimensions', { treeDataProvider: dimensionItemProvider });
-        vscode.window.createTreeView<ProcessItem>('package-processes', { treeDataProvider: processeItemProvider });
-        vscode.window.createTreeView<ChoreItem>('package-chores', { treeDataProvider: choreItemProvider });
-    });
-
-    context.subscriptions.push(loadAllTree)
+    //context.subscriptions.push(loadAllTree)
 }
 
 export function deactivate() { }
