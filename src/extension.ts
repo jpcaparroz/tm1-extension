6e6fc963e249b62c08eed01a3e00ae001cf21228
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2024-05-01 10:46:36 -0300

    merge with dev-fp

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,12 @@ import * as credentials from './assets/examples/applications.json';
 import { authenticate } from './services/authenticator';
 import { TreeService } from './services/tree-service';
 
-const connection = credentials.GO_New_Stores
+const connection = credentials.planner
 
 export async function activate(context: ExtensionContext) {
     const tm1Service: TM1Service = await authenticate(connection);
     const treeSerivce = new TreeService(tm1Service);
 
-    //context.subscriptions.push(loadAllTree)
 }
 
 export function deactivate() { }
